4388785dc50ac6215656fdc4954d022c18b212d4
Erick Jeronimo <erickjeronimo@gmail.com>
2017-05-29 17:28:13 -0300

    Adicionando componente de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
+import { RouterLink } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import { LoginComponent } from './login/login.component';
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+        },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
